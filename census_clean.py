31930024dfe49cc403f99ca974866619be069d98
flapjackstan <elm.s.camargo@gmail.com>
2020-03-26 13:03:14 -0700

    adding new york clean

---
+++ census_clean.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 12:38:08 2020
+
+@author: elmer camargo
+"""
+
+import os
+import pandas as pd
+
+os.chdir('C:/Users/elmsc/Documents/gis/che-covid19')
+
+path = 'data/census/new_york/'
+
+os.walk(path)
