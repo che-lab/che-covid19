8684987434826f7ae3eabba22499acb4f4ef45e8
Geoffrey Hughes <46589744+geoffreyhughes@users.noreply.github.com>
2020-04-09 19:45:28 -0700

    updated covid_day_clean2.py
    This is a final working version! Will continue to update to clean columns as new information comes in

--- covid_day_clean2.py
+++ covid_day_clean2.py
@@ -2,8 +2,16 @@ import os
 import pandas as pd
 
 
-os.chdir('/Users/geoffreyhughes/Projects/Work/COVID-19/che-covid19/data')
+# Change to directory with the CSV files
+os.chdir('/Users/geoffreyhughes/Projects/Work/COVID-19/che-covid19/data/csse_covid_19_daily_reports')
 
+
+# Clear previous CSVs if they exist
+if os.path.isfile('raw_data.csv'):
+    os.remove('raw_data.csv')
+
+if os.path.isfile('clean_data.csv'):
+    os.remove('clean_data.csv')
 
 
 # Read all CSV data
@@ -11,8 +19,7 @@ all_csv = [file_name for file_name in os.listdir(os.getcwd()) if '.csv' in file_
 li = []
 
 
-# Combine all CSV data. If missing an attribute, leave blank (null)
-# e.g. 'Long_' -> 'Longitude'
+# Combine all CSV data. If missing an attribute, leave blank (NaN)
 for filename in all_csv:
     df = pd.read_csv(filename, index_col=None, header=0, parse_dates=True, infer_datetime_format=True)
     li.append(df)
@@ -25,32 +32,34 @@ frame.to_csv('raw_data.csv', index=False)
 
 
 # Check for similar attributes and combine them
-# e.g. 'Long_' -> 'Longitude'
+# Using x_y naming convention (underscores)
+# e.g. 'Long_' -> 'Longitude'; 'Country/Region' -> 'Country_Region'
 print(len(li))
-
-
 print(len(li))
 print(len(frame))
 
+# 'Country/Region' -> 'Country_Region'
+frame['Country_Region'] = frame['Country/Region'].fillna('') + frame['Country_Region'].fillna('')
+del frame['Country/Region']
 
-# goes through global cases dict and filters for only US cases
-# I use 2 try excepts because col names change between using '/' and '_'
+# 'Province/State' -> 'Province_State'
+frame['Province_State'] = frame['Province/State'].fillna('') + frame['Province_State'].fillna('')
+del frame['Province/State']
 
-# us_dict = {}
-# for i in range(0,len(global_dict)):
-#     temp = global_dict[csv[i]]
-#     try:
-#         us_temp = temp[temp['Country/Region'] == 'US']
-#     except:
-#         try:
-#             us_temp = temp[temp['Country_Region'] == 'US']
-#         except: print("error")
-#     us_dict[csv[i]] = us_temp
-# del(us_temp,temp)
+# 'Lat' -> 'Latitude'
+frame['Latitude'] = frame['Lat'].fillna(0) + frame['Latitude'].fillna(0)
+del frame['Lat']
+frame['Latitude'].replace([0, 0.0], '', inplace=True)
+
+# 'Long_' -> 'Longitude'
+frame['Longitude'] = frame['Long_'].fillna(0) + frame['Longitude'].fillna(0)
+del frame['Long_']
+frame['Longitude'].replace([0, 0.0], '', inplace=True)
 
 
+if os.path.isfile('clean_data.csv'):
+    os.remove('clean_data.csv')
+frame.to_csv('clean_data.csv', index=False)
+
+# Delete working variables; Done
 del(li, df, frame)
-
-
-#github test
-
